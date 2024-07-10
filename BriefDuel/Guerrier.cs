e30a8d57a477228dd701d17a63e4dda96d20f976
Achap05 <chapon.art@gmail.com>
2024-07-10 17:01:28 +0200

    feat: init BriefDuel

---
+++ BriefDuel/Guerrier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Guerrier
+    {
+        #region "Attributs"
+        private string _nom;
+        private int _pv;
+        private int _nbrAttaques;
+        #endregion
+
+        #region "Propriétés"
+        public string Nom { get => _nom; set => _nom = value; }
+        public int PV { get => _pv; set => _pv = value; }
+        public int NbrAttaques { get => _nbrAttaques; set => _nbrAttaques = value; }
+        #endregion
+
+        // Méthodes
+
+        public Guerrier(string nom,int pv,int nbrAttaques)
+        {
+            Nom = nom; 
+            PV = 100;
+            NbrAttaques = 1;
+        }
+}
+
+
---
+++ BriefDuel/Nain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Nain : Guerrier
+{
+    private int _bouclier;
+    public Nain(string name, int pv, int nbrAttaques) :
+        base(name, pv, nbrAttaques);
+    {
+        
+    }
+    }
+    
+
