003d0dc572cbf39c8d9d4a2295a62122d5df7de6
Achap05 <chapon.art@gmail.com>
2024-07-16 09:07:41 +0200

    chore: edit BriefDuel

--- BriefDuel/Program.cs
+++ BriefDuel/Program.cs
@@ -1,19 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel.Design;
 
-Console.WriteLine("Hello, World!");
-
 
 Console.WriteLine("Choisissez:");
 Console.WriteLine("---[1] Choisir un personnage---");
-Console.WriteLine("---[2] Choisir un adversaire---");
-Console.WriteLine("---[3] Refaire un combat---");
+Console.WriteLine("---[2] Créer un personnage---");
+Console.WriteLine("---[3] Faire un combat---");
+Console.WriteLine("---[4] supprimer un personnage---");
 
 
-static void Menu() 
-{
 
-}
+
 
 Guerrier g1 = new Guerrier("Arthur", 30, 2);
 Guerrier g2 = new Guerrier("Renaud", 30, 1);
@@ -34,3 +31,37 @@ gNain.AfficherInfos();
 gElfe.Attaque();
 gElfe.AfficherInfos();
 
+bool End = false;
+while (!End)
+{
+        switch (Console.ReadLine())
+        {
+            case "1":
+            Guerrier CréationPerso()
+            {
+                string nom;
+                int pv;
+                int nbrAttaques;
+            }
+            break;
+
+        case "2":
+            
+            break;
+
+        case "3":
+            
+            break;
+
+        default:
+            Console.WriteLine("Erreur ce choix n'est pas reconnu");
+          
+            break;
+        }
+
+
+static void Menu()
+{
+
+
+}
